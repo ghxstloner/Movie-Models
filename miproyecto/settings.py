ac58c8f489503d0ff3abc45842e59aa39cd4de63
Yoiner Moreno <yoinerdeyhan@gmail.com>
2023-01-27 15:40:53 -0500

    Update setting.py
    Deleting the secret key of Django for the public

--- miproyecto/settings.py
+++ miproyecto/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x@3jx*u0vhf(p#4f^-caqdt+k&*rayqa$o9i%au=80ra5(l2cz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
